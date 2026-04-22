18582d9bdaadbbcfacfadbba3fac0b27a4f9e622
Fiza <fiza.workspace8@gmail.com>
2026-04-22 21:43:40 +0500

    Remove login system safely

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Lenis from 'lenis'
 
-import { AuthProvider } from './context/AuthContext'
 import { CartProvider } from './context/CartContext'
 
 import Navbar from './components/Navbar'
@@ -11,12 +10,9 @@ import Footer from './components/Footer'
 // Pages
 import Home from './pages/Home'
 import Menu from './pages/Menu'
-import Login from './pages/Login'
 import Checkout from './pages/Checkout'
 import Confirmation from './pages/Confirmation'
 import OrderHistory from './pages/OrderHistory'
-
-import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   useEffect(() => {
@@ -32,30 +28,30 @@ function App() {
     }
 
     requestAnimationFrame(raf)
+
     return () => lenis.destroy()
   }, [])
 
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Router>
-          <div className="relative min-h-screen bg-[#0a0a0a] text-white">
-            <Navbar />
-            <main>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                <Route path="/menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} />
-                <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-                <Route path="/confirmation" element={<ProtectedRoute><Confirmation /></ProtectedRoute>} />
-                <Route path="/orders" element={<ProtectedRoute><OrderHistory /></ProtectedRoute>} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </Router>
-      </CartProvider>
-    </AuthProvider>
+    <CartProvider>
+      <Router>
+        <div className="relative min-h-screen bg-[#0a0a0a] text-white">
+          <Navbar />
+
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/confirmation" element={<Confirmation />} />
+              <Route path="/orders" element={<OrderHistory />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   )
 }
 
